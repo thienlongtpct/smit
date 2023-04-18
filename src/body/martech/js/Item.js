e1dd3dd9c0eefca6c99eb42b1732a7c831d126d8
Vo Minh Thien Long <thienlongtpct@gmail.com>
2023-04-18 18:30:52 +0300

    change name

--- src/body/martech/js/Item.js
+++ src/body/martech/js/Item.js
@@ -3,7 +3,7 @@ import itemsStyles from '../styles/Items.module.css';
 
 const Item = ({avatar, title, desc, desc2}) => (
     <div className={`${itemsStyles.item} ${commonStyles.itemBox}`}>
-        <img className={itemsStyles.avatar} src={process.env.PUBLIC_URL + "/images/index/" + avatar + ".png"} alt=""/>
+        <img className={itemsStyles.avatar} src={process.env.PUBLIC_URL + "/images/index/" + avatar + ".jpg"} alt=""/>
         <div className={itemsStyles.title}>{title}</div>
         <div className={itemsStyles.desc}>{desc}</div>
         <div className={itemsStyles.desc2}>{desc2}</div>
--- src/body/martech/js/Items.js
+++ src/body/martech/js/Items.js
@@ -6,20 +6,20 @@ const Items = () => {
     const items = [
         {
             avatar: "martech-avatar-1",
-            title: "Duong Manh Kien",
-            desc: "Theanh28 Entertainment Deputy Director",
+            title: "Micheal Arteta",
+            desc: "Head of SSA Entertaiment Department",
             desc2: "In the community of Facebook Ads advertisers, SMIT always shows us the enthusiasm and professionalism in each service or product that SMIT is providing. Through these things, we I understand that, SMIT has and is having a young, enthusiastic and passionate staff in the field of software engineering."
         },
         {
             avatar: "martech-avatar-2",
-            title: "Tinh Nguyen",
-            desc: "Co-Founder Ladipage Vietnam",
+            title: "Philipo Inzzagi",
+            desc: "Co-founder Smit company",
             desc2: "After a period of experience with Ads Check by SMIT, I confirm that this is a superior tool trusted and trusted by advertisers. Whether individuals or businesses should choose products because comprehensive support to run effective ads. You can easily build smarter Ads campaigns with outstanding functions!"
         },
         {
             avatar: "martech-avatar-3",
-            title: "Luu Thien An",
-            desc: "Founder HAS Agency",
+            title: "Micheal Jordan",
+            desc: "Founder Smit Agency",
             desc2: "Since using Ads Check by SMIT, I have noticed a clear effect when checking all information simply, quickly and easily. Ad accounts are also protected. and give the cause of disabling the display completely free of charge."
         }
     ];
